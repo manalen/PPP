a2bd61f3d6a1b69609cebd05be5462888c06449d
manalen <61988178+manalen@users.noreply.github.com>
2020-10-29 21:24:21 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import KFold
 %matplotlib inline
+
+def draw_heatmap(data):
+    sns.heatmap(data.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+
+def convert_cast(df,categorical_cols):
+   for col_name in categorical_cols:
+       df[col_name]=df[col_name].astype("object")
+   return df
+colonnes=["Survived","Pclass"]
+train=convert_cast(train,colonnes)
+train.info()

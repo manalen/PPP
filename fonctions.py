bfcce294c8e3de420ca1d0c98bbc11cdb3338164
manalen <61988178+manalen@users.noreply.github.com>
2020-10-30 21:16:37 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -12,3 +12,18 @@ def convert_cast(df,categorical_cols):
    for col_name in categorical_cols:
        df[col_name]=df[col_name].astype("object")
    return df
+
+def input_missing_values(df):
+    for col in df.columns:
+        if (df[col].dtype is float) or (df[col].dtype is int):
+            df[col]=df[col].fillna(df[col].median())
+        if (df[col].dtype == object):
+            df[col]=df[col].fillna(df[col].mode()[0].split(" ")[0])
+    return df
+
+def parse_model(X, use_columns):
+    if "Survived" not in X.columns :
+        raise ValueError("target column survived should belong to df")
+    target=X["Survived"]
+    X=X[use_columns]
+    return X, target
